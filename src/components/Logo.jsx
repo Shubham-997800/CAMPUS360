aa37e5332ecef13fab53e58740f012685cd7619b
Shubham-997800 <shubhamkumar997800@gmail.com>
2026-06-26 21:21:12 +0530

    Replace JPEG logo with SVG N-monogram icon matching NexCampus brand

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,14 +1,21 @@
 import { useTheme } from '../context/ThemeContext'
-import whiteLogo from '../assets/WHITE.jpeg'
-import darkLogo from '../assets/DARK.jpeg'
 
-export default function Logo({ className = 'h-8 w-auto' }) {
+export default function Logo({ size = 32, inverted = false }) {
   const { dark } = useTheme()
+
+  if (inverted) {
+    return (
+      <svg width={size} height={size} viewBox="0 0 32 32" fill="none">
+        <rect width="32" height="32" rx="8" fill="white" />
+        <path d="M9 23V9H12.5L18.5 18.5V9H22V23H18.5L12.5 13.5V23H9Z" fill="#6C5CE7" />
+      </svg>
+    )
+  }
+
   return (
-    <img
-      src={dark ? darkLogo : whiteLogo}
-      alt="NexCampus"
-      className={`object-contain ${className}`}
-    />
+    <svg width={size} height={size} viewBox="0 0 32 32" fill="none">
+      <rect width="32" height="32" rx="8" className="fill-[#6C5CE7] dark:fill-[#7C5CFF]" />
+      <path d="M9 23V9H12.5L18.5 18.5V9H22V23H18.5L12.5 13.5V23H9Z" className="fill-white" />
+    </svg>
   )
 }
